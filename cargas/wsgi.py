b58cddc68a5e46392f2bed1f01ba01fcecbfa9f8
ariel-brassesco <ariel.brassesco@gmail.com>
2020-10-09 12:18:52 -0300

    First commit

---
+++ cargas/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for cargas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+import dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+try:
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except Exception as e:
+    print('no .env file, using default os env')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cargas.settings')
+
+application = get_wsgi_application()
