8b3b97a09cb2e5794ee2941cada68bb5a7d5ee55
Ariel Brassesco <ariel.brassesco@gmail.com>
2021-05-04 22:58:37 -0300

    Add search container bar

--- frontend/src/pages/ClientHomePage.tsx
+++ frontend/src/pages/ClientHomePage.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { useSelector } from "react-redux";
 import lodash from "lodash";
 // Import Componentes
 import { ClientOrder } from "../components/ClientOrder";
+import { SearchBar } from "../components/SearchBar";
+
 // Import Getters
 import { getOrders } from "../reducers/clientReducer";
 // Import Types
 import { Order } from "../types/order";
 
-const ClientHomePage: React.FC = () => {
+const ClientHomePage: FC = () => {
+  const [search, setSearch] = useState("");
   const orders: Order[] = useSelector((state: any) => getOrders(state));
+  console.log(search);
   return (
     <div className="mt-3">
       {lodash.isEmpty(orders) ? (
         <h1 className="title has-text-centered">No hay Cargas para Mostrar</h1>
       ) : (
-        orders.map((o: Order, idx: number) => (
-          <ClientOrder key={idx} order={o} />
-        ))
+        <>
+          <SearchBar value={search} onSearch={setSearch} />
+          {orders
+            .filter((o: Order) =>
+              o.order.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((o: Order, idx: number) => (
+              <ClientOrder key={idx} order={o} />
+            ))}
+        </>
       )}
     </div>
   );
